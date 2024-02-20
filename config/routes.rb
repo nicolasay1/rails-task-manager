7023f3055d8150e76c8d0ec7920bbbf952bd5ff0
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-02-20 14:23:33 +0000

    Created new action in routes, controller, and html

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
